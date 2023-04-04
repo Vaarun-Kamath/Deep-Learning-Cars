531eea0eabd5a0ab5677f4e500a01b26de1d817b
RockerBot <rockerbotmail@gmail.com>
2023-04-04 17:00:58 +0530

    Made track and collision detection

--- cargame.py
+++ cargame.py
@@ -1,6 +1,8 @@
-import pygame
+import math as maths
 import random
-import math
+import numpy as np
+import pygame
+import cv2 as cv
 
 
 # initialize pygame
@@ -22,9 +24,15 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+# GLOBALS
+track_img_path = "track.png"
+running = False
+caravan = []
+
 # define car class
 class Car:
     def __init__(self, x, y, image):
+        self.start = x,y
         self.x = x
         self.y = y
         self.image = image
@@ -39,6 +47,41 @@ class Car:
 
     def update(self):
         # update car position
+        self.x += self.speed * maths.cos(self.angle)
+        self.y += self.speed * maths.sin(self.angle)
+        # wrap-around
+        if self.x < -self.width:    self.x = screen_width
+        elif self.x > screen_width: self.x = -self.width
+        if self.y < -self.height:   self.y = screen_height
+        elif self.y > screen_height: self.y = -self.height
+
+        return self
+    
+    def reset(self):
+        print('reset!')
+        self.x, self.y = self.start
+        self.speed = 0
+        self.acceleration = 0.2
+        self.max_speed = 10
+        self.angle = 0
+        self.rotate_speed = 1
+        self.max_angle = 45
+
+    def draw(self):
+        rotated_image = pygame.transform.rotate(self.image, -self.angle*180/maths.pi)
+        screen.blit(rotated_image, (self.x - rotated_image.get_width()/2, self.y - rotated_image.get_height()/2))
+
+        return self
+    
+    def __repr__(self) -> str:
+        return f"Car({self.x},{self.y})"
+
+class Player(Car):
+    def __init__(self, x, y, image):
+        Car.__init__(self, x, y, image)
+    
+    def update(self):
+        # update car position
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             self.speed += self.acceleration
@@ -48,34 +91,26 @@ class Car:
             self.speed -= self.acceleration
             if self.speed < -self.max_speed:
                 self.speed = -self.max_speed
-        self.x += self.speed * math.cos(self.angle)
-        self.y += self.speed * math.sin(self.angle)
         # handle car rotation
         if keys[pygame.K_RIGHT]:
             self.angle += 0.05
         elif keys[pygame.K_LEFT]:
             self.angle -= 0.05
-        if self.x < -self.width:
-            self.x = screen_width
-        elif self.x > screen_width:
-            self.x = -self.width
-        if self.y < -self.height:
-            self.y = screen_height
-        elif self.y > screen_height:
-            self.y = -self.height
-
-
+        
+        return super().update()
+    
     def draw(self):
-        rotated_image = pygame.transform.rotate(self.image, -self.angle*180/math.pi)
-        screen.blit(rotated_image, (self.x - rotated_image.get_width()/2, self.y - rotated_image.get_height()/2))
+        return super().draw()
 
 # define track class
 class Track:
-    def __init__(self, num_points, point_range):
+    def __init__(self, num_points, point_range, dim):
         self.num_points = num_points
         self.point_range = point_range
         self.points = []
+        self.track_points:np.array = []
         self.generate_points()
+        self.image = self.reload_image(dim)
         
     def generate_points(self):
         # generate random points
@@ -87,47 +122,91 @@ class Track:
     def draw(self):
         # draw track using generated points
         # pygame.draw.lines(screen, white, True, self.points, 10)
+        if not self.image:
+            self.reload_image((screen_width,screen_height))
+        screen.blit(self.image, (0,0 #(screen_width-self.image.get_width())//2,(screen_height-self.image.get_height())//2
+                    ))
+        # pygame.draw.circle(screen, (0,200,0), (screen_width//2,screen_height//2), screen_height//2)
+        # pygame.draw.circle(screen, (255,255,255), (screen_width//2,screen_height//2), screen_height//4)
 
-        pygame.draw.circle(screen, (0,200,0), (screen_width//2,screen_height//2), screen_height//2)
-        pygame.draw.circle(screen, (255,255,255), (screen_width//2,screen_height//2), screen_height//4)
         # se2 = 70
         # pygame.draw.ellipse(screen, (0,0,0), (0,0,screen_width,screen_height))
         # pygame.draw.ellipse(screen, (255,255,255), (10,10,screen_width-20,screen_height-20))
         # pygame.draw.ellipse(screen, (0,240,0), (se2,se2,screen_width-(se2*2),screen_height-(se2*2)))
 
+    def reload_image(self, dim:tuple, /)-> pygame.Surface:
+        assert isinstance(dim,tuple) and len(dim) == 2, "(width, height)"
+        global track_img
+        img = cv.imread(track_img_path)
+        re_sized_img = cv.resize(img, dim)
+        self.track_points = cv.Canny(re_sized_img, 125, 175)
+        track_img = cv.cvtColor(self.track_points, cv.COLOR_GRAY2RGB)
+        return pygame.image.frombuffer(track_img.tobytes(), track_img.shape[1::-1], "RGB")
+    
+    def detect_collisions(self, caravan:list):
+        for car in caravan:
+            if(self.has_colided(car)):
+                car.reset()
+    
+    def has_colided(self, car:Car, /)-> bool:
+        rect = cv.boxPoints(((car.x,car.y), (car.width,car.height), car.angle*180/maths.pi))
+        y_key = lambda x:x[1]
+        x_key = lambda x:x[0]
+        y_range = maths.floor( min(rect, key=y_key)[1] ), maths.ceil( max(rect, key=y_key)[1] )
+        x_range = maths.floor( min(rect, key=x_key)[0] ), maths.ceil( max(rect, key=x_key)[0] )
+        for y in range(*y_range):
+            for x in range(*x_range):
+                if cv.pointPolygonTest(rect, (x, y), True) >= 0:
+                    if self.track_points[y,x]:return True
+        return False
 
 
-# load car image
-car_image = pygame.image.load("car.png")
+def main():
+    global running
+    # load car image
+    car_image = pygame.image.load("car.png")
 
-# create car object
-car = Car(screen_width/2, screen_height/2, car_image)
+    # create car object
+    car = Player(screen_width/2-50, screen_height/2, car_image)
+    caravan.append(car)
 
-# create track object
-track = Track(20, (100, 700))
+    # create track object
+    track = Track(20, (100, 700), dim=(screen_width,screen_height))
 
-# game loop
-running = True
-while running:
-    # event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # game loop
+    running = True
+    while running:
+        # event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_r:
+                    track.reload_image((screen_width, screen_height))
 
-    # update car and track
-    car.update()
+        # update car and track
+        car.update()
 
-    # draw background and track
-    screen.fill((255, 255, 255))
-    track.draw()
+        #keep the cars on the track
+        track.detect_collisions(caravan)
 
-    # draw car
-    car.draw()
+        # draw background and track
+        screen.fill((255, 255, 255))
+        track.draw()
 
-    # update display
-    pygame.display.update()
+        # draw car
+        car.draw()
 
-    # set frame rate
-    clock.tick(60)
+        # update display
+        pygame.display.update()
 
-pygame.quit()
+        # set frame rate
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
